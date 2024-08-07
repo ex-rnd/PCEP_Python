32ea0f452825a064ac189f3d245e6b8c11d03344
James Kilonzo <jkilonzo@com21.co.ke>
2024-08-07 09:51:21 +0300

    Module 1

---
+++ Module_2.py
@@ -0,0 +1,98 @@
+print("Hello, world!")
+
+print("Hello, Python!")
+# print("Greg")
+# print(Greg)
+# print"Greg"
+# print('Greg')
+# print("Greg") print("Python")
+# ...</sampleSolution>
+
+print("The itsy bitsy spider climbed up the waterspout.")
+print("Down came the rain and washed the spider out.")
+
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+print("My name is", "Python.")
+print("Monty Python.")
+
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+print("My name is ", end="")
+print("Monty Python.")
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in")
+print("Python")
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print("My\nname\nis\nBond.", end=" ")
+print("James Bond.")
+
+# print(sep="&", "fish", "chips")
+
+print("2")
+print(2)
+
+print(0o123)
+
+print(0x123)
+
+print("I like \"Monty Python\"")
+
+print('I like "Monty Python"')
+
+print('I\'m Monty Python.')
+
+print(True > False)
+print(True < False)
+
+print(2+2)
+
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+
+

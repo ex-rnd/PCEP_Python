6f216478e65d482b5f4f0a17c7ce51b49e709f4f
James Kilonzo <jkilonzo@com21.co.ke>
2024-08-09 15:32:39 +0300

    Updated Module 2: Operators

--- Module_2.py
+++ Module_2.py
@@ -94,5 +94,34 @@ print(6 // 3.)
 print(6. // 3)
 print(6. // 3.)
 
+print(-6 // 4)
+print(6. // -4)
+
+print(14 % 4)
+
+print(-4 + 4)
+print(-4. + 8)
+
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+
+print(12 % 4.5)
+
+print(+2)
+
+print(9 % 6 % 2)
+
+print(2 * 3 % 5)
+    
+print((2 ** 4), (2 * 4.), (2 * 4))
+
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+
+
+
+
+
+
 
 

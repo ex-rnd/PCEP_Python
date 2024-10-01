c5c71dffbb14dbe9ff35a8cfc3caf21284563fc0
James Kilonzo <ever.ex.rnd@gmail.com>
2024-10-01 15:46:50 +0300

    The great IF statement

--- Module_3.py
+++ Module_3.py
@@ -4,6 +4,35 @@ print(var == 0)
 var = 1  # Assigning 1 to var
 print(var == 0)
 
+# n = int(input("Enter a number: "))
+# print(n >= 100)
+
+
+
+# x = int(input("Input Num 1: "))
+
+# y = int(input("Input Num 2: "))
+
+# if x > y:
+#     largeNum = x
+    
+# else:
+#     largeNum = y
+    
+# print("The largest number, is:" + str(largeNum))
+
+# x = input("Please, input the name of a flower: ")
+
+# if (x == "Spathiphyllum"):
+#     print(f'{x} is the best plant ever!')
+# elif (x == pelargonium):
+#     print(f'Spathiphyllum! Not pelargonium!')
+# else:
+#     print(f'No, I want a big Spathiphyllum!')
+
+
+
+
 
 
 
